8cf63bcef1fff6e4c6db060e2aa9ce3be81d4119
Piyush Acharya <acharyapiyush1@outlook.com>
2022-12-03 15:07:35 -0800

    Finish Part 2

--- day1/part1.java
+++ day1/part1.java
@@ -1,39 +1,58 @@
-package day1;
+package Day1;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 
-public class part1 {
+public class Part1 {
     private static String getText(Path filename) throws IOException {
         return Files.readString(filename);
     }
 
-    public static void main(String[] args) throws IOException {
-        Path filepath = Path.of("C:\\Users\\achar\\OneDrive\\Documents\\GitHub\\AdventOfCode2022\\input.txt");
-        String input = getText(filepath);
+    private static String[] getPeople(String input) {
+        return input.split("\n\n");
+    }
 
-        ArrayList<Integer> calsPerPerson = new ArrayList<>();
+    private static int[] getTotalCals(String[] people) {
+        int[] calsPerPerson = new int[people.length];
 
-        String[] people = input.split("\n\n");
         for (int i = 0; i < people.length; i++) {
             String[] individualCalories = people[i].split("\n");
-
             int totalCals = 0;
             for (int j = 0; j < individualCalories.length; j++) {
                 totalCals += Integer.parseInt(individualCalories[j]);
             }
 
-            calsPerPerson.add(totalCals);
+            calsPerPerson[i] = totalCals;
         }
 
+        return calsPerPerson;
+    }
+
+    private static int findMostCals(int[] calsPerPerson) {
         int mostCals = 0;
-        for (int i = 0; i < calsPerPerson.size(); i++) {
-            if (calsPerPerson.get(i) > mostCals) {
-                mostCals = calsPerPerson.get(i);
+        for (int i = 0; i < calsPerPerson.length; i++) {
+            if (calsPerPerson[i] > mostCals) {
+                mostCals = calsPerPerson[i];
             }
         }
+
+        return mostCals;
+    }
+
+    public static void main(String[] args) throws IOException {
+        // Get the file path and read the text
+        Path filepath = Path.of("C:\\Users\\achar\\OneDrive\\Documents\\GitHub\\AdventOfCode2022\\day1\\input.txt");
+        String input = getText(filepath);
+
+        // Split the input into an array of people
+        String[] people = getPeople(input);
+
+        // Add up the calories for each person
+        int[] calsPerPerson = getTotalCals(people);
+
+        // Find the person with the most calories
+        int mostCals = findMostCals(calsPerPerson);
 
         System.out.println(mostCals);
     }
