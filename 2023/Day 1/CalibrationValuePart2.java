59e0e8b98a73290724079aa0a0515c2c2c59b72f
Piyush Acharya <acharyapiyush1@outlook.com>
2023-12-18 13:08:17 -0800

    Complete Day 1

--- 2023/Day 1/CalibrationValuePart2.java
+++ 2023/Day 1/CalibrationValuePart2.java
@@ -4,13 +4,23 @@ import java.util.*;
 
 public class CalibrationValuePart2 {
     public static void main(String[] args) throws FileNotFoundException {
-        File input = new File("2023/Day 1/input.txt");
+        File input = new File("2023\\Day 1\\input.txt");
         Scanner sc = new Scanner(input);
 
         ArrayList<Integer> addList = new ArrayList<>();
 
         while (sc.hasNextLine()) {
             String currLineString = sc.nextLine();
+            
+            currLineString = currLineString.replace("one", "o1e");
+            currLineString = currLineString.replace("two", "t2");
+            currLineString = currLineString.replace("three", "3");
+            currLineString = currLineString.replace("four", "4");
+            currLineString = currLineString.replace("five", "5");
+            currLineString = currLineString.replace("six", "6");
+            currLineString = currLineString.replace("seven", "7");
+            currLineString = currLineString.replace("eight", "8t");
+            currLineString = currLineString.replace("nine", "9");
 
             // clean string
             ArrayList<Character> currLineList = new ArrayList<>();
@@ -20,35 +30,9 @@ public class CalibrationValuePart2 {
 
             ArrayList<Integer> currLineNumList = new ArrayList<>();
 
-            String spelledOut = "";
             for (char c : currLineList) {
                 if ((c + "").matches("[0-9]+")) {
                     currLineNumList.add(Integer.parseInt(c + ""));
-                    spelledOut = "";
-                } else if (spelledOut.matches("zero|one|two|three|four|five|six|seven|eight|nine")) {                    
-                    if (spelledOut.contains("zero")) {
-                        currLineNumList.add(0);
-                    } else if (spelledOut.contains("one")) {
-                        currLineNumList.add(1);
-                    } else if (spelledOut.contains("two")) {
-                        currLineNumList.add(2);
-                    } else if (spelledOut.contains("three")) {
-                        currLineNumList.add(3);
-                    } else if (spelledOut.contains("four")) {
-                        currLineNumList.add(4);
-                    } else if (spelledOut.contains("five")) {
-                        currLineNumList.add(5);
-                    } else if (spelledOut.contains("six")) {
-                        currLineNumList.add(6);
-                    } else if (spelledOut.contains("seven")) {
-                        currLineNumList.add(7);
-                    } else if (spelledOut.contains("eight")) {
-                        currLineNumList.add(8);
-                    } else if (spelledOut.contains("nine")) {
-                        currLineNumList.add(9);
-                    }
-                } else {
-                    spelledOut += c;
                 }
             }
 
